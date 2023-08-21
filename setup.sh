1e2cb56487ee0e49d052c4a1c60e850053e7e864
Mitchell Sayre <sayremitch@gmail.com>
2023-08-21 10:52:32 -0400

    Cuda keyring download condition

--- setup.sh
+++ setup.sh
@@ -39,8 +39,11 @@ sudo apt-get -y install ca-certificates curl gnupg
 sudo apt-get install -y gcc wget
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
 sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/cuda-repo-ubuntu2204-12-2-local_12.2.1-535.86.10-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2204-12-2-local_12.2.1-535.86.10-1_amd64.deb
+CUDA_KEYRING=cuda-repo-ubuntu2204-12-2-local_12.2.1-535.86.10-1_amd64.deb
+if [ ! -e "$CUDA_KEYRING" ]; then
+    wget https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/$CUDA_KEYRING
+fi
+sudo dpkg -i $CUDA_KEYRING
 sudo cp /var/cuda-repo-ubuntu2204-12-2-local/cuda-*-keyring.gpg /usr/share/keyrings/
 sudo apt-get update
 sudo apt-get -y install cuda
